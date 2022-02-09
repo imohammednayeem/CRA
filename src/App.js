2d37f39ad4a36157d648d7c4d9578789a724cae0
Mohammed Nayeem <nayeem.gmit@gmail.com>
2022-02-09 21:05:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './DesignPatterns/App.css';
+import SplitScreen from './DesignPatterns/SplitScreen';
+
+const LeftHandComponent = () => {
+  return (
+    <div>
+      <h1 style={{backgroundColor : 'green'}}>Left Hand Component</h1>
+    </div>
+  );
+}
+const RightHandComponent = () => {
+  return (
+    <div>
+      <h1 style={{backgroundColor : 'red'}}>Right Hand Component</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplitScreen left={LeftHandComponent} right={RightHandComponent}/>
     </div>
   );
 }
